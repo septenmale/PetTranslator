0eb972282df32a604b9a0ad6c84a197260f5b3d0
septenmale <zavhorodnii.viktor@icloud.com>
2025-02-11 15:04:53 +0100

    Redesign tabBar

--- PetTranslator/Controllers/TabBarController.swift
+++ PetTranslator/Controllers/TabBarController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let tabBarWidth: CGFloat = 216
+        let tabBarHeight: CGFloat = 82
+        let bottomPadding: CGFloat = 46
+        
+        let newFrame = CGRect(
+            x: (view.frame.width - tabBarWidth) / 2,
+            y: view.frame.height - tabBarHeight - bottomPadding,
+            width: tabBarWidth,
+            height: tabBarHeight
+        )
+        
+        tabBar.frame = newFrame
+        
+        tabBar.backgroundColor = .white
+        tabBar.layer.cornerRadius = 16
+        tabBar.layer.masksToBounds = true
+        
+        if let items = tabBar.items {
+            for item in items {
+                item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -19)
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
