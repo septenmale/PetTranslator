77a22c037eb72ec55802e89bb859fc4f85d55e55
septenmale <zavhorodnii.viktor@icloud.com>
2025-02-13 17:52:27 +0100

    Complete ResultVC

--- PetTranslator/Controllers/ResultViewController + ResultViewControllerDelegate .swift
+++ PetTranslator/Controllers/ResultViewController + ResultViewControllerDelegate .swift
@@ -32,10 +32,15 @@ final class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.applyGradient()
-        resultLabel.text = translatedText
-        petImageView.image = selectedImage
         
+        configureView()
+        setUpViews()
+        setupRepeatContainerView()
+        setupResultContainerView()
         setupConstraints()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+            self.showRepeatButton()
+        }
     }
     
     private let titleLabel: UILabel = {
@@ -87,6 +92,35 @@ final class ResultViewController: UIViewController {
         return view
     }()
     
+    private let repeatContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "appPurpleColor")
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        view.isHidden = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let repeatImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "repeatImage")
+        view.tintColor = .black
+        view.contentMode = .scaleAspectFit
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let repeatLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Repeat"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private let petImageView: UIImageView = {
         let view = UIImageView()
         view.image = UIImage(named: "dogImage")
@@ -95,6 +129,44 @@ final class ResultViewController: UIViewController {
         return view
     }()
     
+    private func configureView() {
+        resultLabel.text = translatedText
+        petImageView.image = selectedImage
+    }
+    
+    private func setupResultContainerView() {
+        
+        resultContainerView.addSubview(resultLabel)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showRepeatButton))
+        resultContainerView.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    private func setupRepeatContainerView() {
+        repeatContainerView.addSubview(repeatImageView)
+        repeatContainerView.addSubview(repeatLabel)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(repeatButtonDidTap))
+        repeatContainerView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showRepeatButton() {
+        UIView.animate(withDuration: 0.3) {
+            self.repeatContainerView.isHidden = false
+            self.resultContainerView.isHidden = true
+            self.lineView.isHidden = true
+        }
+    }
+    
+    @objc
+    private func repeatButtonDidTap() {
+        dismiss(animated: false) { [weak self] in
+            self?.delegate?.didDismissResultViewController()
+        }
+    }
+    
     @objc
     private func closeDidTap() {
         dismiss(animated: false) { [weak self] in
@@ -102,14 +174,18 @@ final class ResultViewController: UIViewController {
         }
     }
     
-    private func setupConstraints() {
-        
+    private func setUpViews() {
+        view.addSubview(resultContainerView)
+        view.addSubview(repeatContainerView)
         view.addSubview(closeButton)
         view.addSubview(titleLabel)
         view.addSubview(lineView)
-        view.addSubview(resultContainerView)
-        resultContainerView.addSubview(resultLabel)
         view.addSubview(petImageView)
+    }
+    
+    private func setupConstraints() {
+        
+        
         
         
         NSLayoutConstraint.activate([
@@ -135,6 +211,20 @@ final class ResultViewController: UIViewController {
             lineView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 242),
             lineView.bottomAnchor.constraint(equalTo: petImageView.topAnchor, constant: -19.38),
             
+            repeatContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            repeatContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 49),
+            repeatContainerView.bottomAnchor.constraint(equalTo: petImageView.topAnchor, constant: -50),
+            repeatContainerView.widthAnchor.constraint(equalToConstant: 291),
+            repeatContainerView.heightAnchor.constraint(equalToConstant: 54),
+            
+            repeatImageView.centerYAnchor.constraint(equalTo: repeatContainerView.centerYAnchor),
+            repeatImageView.leadingAnchor.constraint(equalTo: repeatContainerView.leadingAnchor, constant: 111.5),
+            repeatImageView.trailingAnchor.constraint(equalTo: repeatContainerView.trailingAnchor, constant: -163.5),
+            
+            repeatLabel.centerYAnchor.constraint(equalTo: repeatContainerView.centerYAnchor),
+            repeatLabel.leadingAnchor.constraint(equalTo: repeatContainerView.leadingAnchor, constant: 137.5),
+            repeatLabel.trailingAnchor.constraint(equalTo: repeatContainerView.trailingAnchor, constant: -111.5),
+            
             petImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             petImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -134),
             
@@ -142,5 +232,3 @@ final class ResultViewController: UIViewController {
     }
     
 }
-
-//настроить кнопку возврата + заблокировать микрфоон пока идет запись.
